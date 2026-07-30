729a70a24b63dceb1a1bbb04494a6974eed3d6f4
Ogooluwa <163356433+Ogooluwa01@users.noreply.github.com>
2026-07-30 09:21:19 +0100

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='shigt_optmization_system',
+    version='1.0.0',
+    author='Mary',
+    author_email='aleyiboogooluwamary@gmail.com',
+    packages= find_packages()
+)
